faa80483efbc88050b1439b67f168bd51d1f8c22
Irebe Alice <155970025+IrebeAlice@users.noreply.github.com>
2024-02-27 13:25:51 +0000

    7 task python

--- python-hello_world/7-edges.py
+++ python-hello_world/7-edges.py
@@ -3,6 +3,6 @@ word = "Holberton"
 first3 = word[:3]
 last2 = word[-2:]
 mid = word[1:-1]
-print(f"First 3 letters: {word_first_3}")
-print(f"Last 2 letters: {word_last_2}")
-print(f"Middle word: {middle_word}")
+print("First 3 letters: {}".format(first3))
+print("Last 2 letters: {}".format(last2))
+print("Middle word: {}".format(mid))
