577ee89a33a8a03b0294fa1c4cab70169fcff17c
Irebe Alice <155970025+IrebeAlice@users.noreply.github.com>
2024-02-27 18:05:13 +0000

    12 task python loops

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -7,4 +7,6 @@ def fizzbuzz():
             print("Fizz ", end="")
         elif number % 5 == 0:
             print("Buzz ", end="")
+        else:
+            print("{} ".format(number), end="")
     
