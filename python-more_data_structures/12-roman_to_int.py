5889c852bf327e83904a36a1daa25575e206f376
IrebeAlice <i.alice@alustudent.com>
2024-03-01 18:15:39 +0200

    12 task python more data

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -3,7 +3,7 @@ def roman_to_int(roman_string):
     if roman_string is None or type(roman_string)is not str:
         return 0
     int_sum = 0
-    converter = dict(I=1, V=5, L=50, C=10, D=50, M=100)
+    converter = dict(I=1, V=5, L=50, C=10, D=500, M=1000)
     for current, next in zip(roman_string, roman_string[1:]):
        if converter[current] >= converter[next]:
           int_sum += converter[current]
