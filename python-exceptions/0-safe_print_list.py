6b7331918dcc8538c9cbe1a3bd1b6e8f8f920b3a
IrebeAlice <i.alice@alustudent.com>
2024-03-01 19:40:37 +0200

    12 task python more data

---
+++ python-exceptions/0-safe_print_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    ret = 0
+    for each in range (x):
+        try:
+            print(f"{my_list[each]}",ends="")
+            ret += 1
+        except:
+            break 
+        print()
+        return(ret)
