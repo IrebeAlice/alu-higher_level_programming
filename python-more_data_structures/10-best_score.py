a1d0b87a30453c64da02601276e07556d3e74216
IrebeAlice <i.alice@alustudent.com>
2024-03-01 17:45:10 +0200

    10 task python  more data

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -2,10 +2,10 @@
 def best_score(a_dictionary):
     if not isinstance(a_dictionary, dict) or len(a_dictionary) == 0:
         return None
-    ret =list(a_dictionary.keys())[0]
+    ret = list(a_dictionary.keys())[0]
     big = a_dictionary[ret]
     for k, v in a_dictionary.items():
         if v > big:
             big = v
             ret = k
-            return(ret) 
+     return(ret) 
