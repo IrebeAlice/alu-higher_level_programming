63a2ab8242e8797b322c2857944d2f7f4ed629be
IrebeAlice <i.alice@alustudent.com>
2024-03-01 19:54:29 +0200

    0 task python exceptions

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -6,6 +6,6 @@ def safe_print_list(my_list=[], x=0):
             print(f"{my_list[each]}", end="")
             ret += 1
         except:
-            break 
+               break 
         print()
         return(ret)
