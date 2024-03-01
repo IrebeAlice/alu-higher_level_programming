c3766c1a88d15c17158908a11889c263afb711df
IrebeAlice <i.alice@alustudent.com>
2024-03-01 19:53:00 +0200

    0 task python exception

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -3,7 +3,7 @@ def safe_print_list(my_list=[], x=0):
     ret = 0
     for each in range (x):
         try:
-            print(f"{my_list[each]}",end="")
+            print(f"{my_list[each]}", end="")
             ret += 1
         except:
             break 
