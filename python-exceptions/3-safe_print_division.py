bfd6d5130b488b85995b81c9606f43ec81ac68bd
IrebeAlice <i.alice@alustudent.com>
2024-03-01 20:23:05 +0200

    3 task python exceptions

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
+
+
 def safe_print_division(a, b):
     try:
-        result =a / b
+        result = a / b
     except(TypeError, ZeroDivisionError):
         result = None
     finally:
-        print("inside result: {}".format(result))
-        return result
-    
+        print("Inside result: {}".format(result))
+    return result
