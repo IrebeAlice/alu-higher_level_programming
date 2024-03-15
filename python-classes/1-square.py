ce8671da8b636b821a04685bd5d1224364c85ab5
IrebeAlice <i.alice@alustudent.com>
2024-03-15 13:40:56 +0200

    1 task python classes

---
+++ python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""the class spuare"""
+
+class Square:
+    """ defining the class square, """
+    def __unit__(self, size):
+      """ defining the size of the square """
+      self.__size = size
+      
