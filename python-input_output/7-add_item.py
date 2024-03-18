264648792c3852db4ff37491bfc3b063c8ab9458
IrebeAlice <i.alice@alustudent.com>
2024-03-18 16:13:34 +0200

    7 task

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -5,9 +5,9 @@ import sys
 
 if __name__ == "__main__":
     save_to_json_file = \
-        __import__('5-save_to_json_file').save_to_json_file
+        __import__('5-save_to_json_file.py').save_to_json_file
     load_from_json_file = \
-        __import__('6-load_from_json_file').load_from_json_file
+        __import__('6-load_from_json_file.py').load_from_json_file
 
     try:
         content = load_from_json_file("add_item.json")
@@ -15,4 +15,3 @@ if __name__ == "__main__":
         content = []
     content.extend(sys.argv[1:])
     save_to_json_file(content, "add_item.json")
-    
