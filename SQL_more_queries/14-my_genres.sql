29eddb063a0a065e582b4ba95e1eb211865c69a3
IrebeAlice <i.alice@alustudent.com>
2024-03-22 20:59:30 +0200

    14 task more queries

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -2,7 +2,7 @@
 SELECT g.name AS genre,
 COUNT(*) AS number_of_shows
 FROM tv_genres AS g
-INNER JOIN tv_show_genres AS tg
+INNER JOIN tv_show_genreS AS tg
 ON g.id = tg.genre_id
 GROUP BY g.name
 ORDER BY number_of_shows DESC;
