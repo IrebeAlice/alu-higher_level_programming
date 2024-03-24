2953ede022fed2216c64ce1d4830d2fefa0a195b
IrebeAlice <i.alice@alustudent.com>
2024-03-24 21:25:30 +0200

    update

--- python-network_0/0-body_size.sh
+++ python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# takes in a URL, sends a request and displays size body of the response
-response=$(curl -s -o temp_body.txt -w "%{size_download}" "$1")
-echo "Response size: $response bytes"
+size=$(curl -s -o temp_body.txt -w "%{size_download}" "$1")
+echo "Response size: $size bytes"
