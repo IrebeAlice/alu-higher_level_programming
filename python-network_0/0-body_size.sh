e36014e027152a5a970a857fe222bc512c8a050d
IrebeAlice <i.alice@alustudent.com>
2024-03-24 21:30:37 +0200

    update

--- python-network_0/0-body_size.sh
+++ python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#size=$(curl -s -o temp_body.txt -w "%{size_download}" "$1")
-echo "Response size: $size bytes"
+# Use curl to send a request to the URL, pipe the output to wc to count the characters (size), and display the result
+curl -s "$1" | wc -c
