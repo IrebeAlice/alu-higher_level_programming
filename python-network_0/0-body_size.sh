a8fd1db73f41db807d803e473ae6aa348e09ec43
IrebeAlice <i.alice@alustudent.com>
2024-03-24 21:35:08 +0200

    update

--- python-network_0/0-body_size.sh
+++ python-network_0/0-body_size.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
-# Check if a URL is provided
-if [ -z "$1" ]; then
-  echo "Usage: $0 <URL>"
-  exit 1
-fi
 # Use curl to send a request to the URL, pipe the output to wc to count the characters (size), and display the result
 curl -s "$1" | wc -c
