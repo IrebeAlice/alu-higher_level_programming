7cb9598bf88dfc43a8b6a6f61e24cdd1f8015a0b
IrebeAlice <i.alice@alustudent.com>
2024-03-24 21:41:43 +0200

    update

--- python-network_0/0-body_size.sh
+++ python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Use curl to send a request to the URL, pipe the output to wc to count the characters (size), and display the result
-curl -s -w "%{size_download}\n" -o /dev/null "$1"
+curl -s -w "%{size_download}\n" -o /dev/null "$1:5000"
