d2b283446be538423fde6b5e2be820d9da68d35b
IrebeAlice <i.alice@alustudent.com>
2024-03-24 20:50:02 +0200

    o task python network

--- python-network_0/0-body_size.sh
+++ python-network_0/0-body_size.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 # takes in a URL, sends a request and displays size body of the response
-curl -s "$1" | wc -c
+url="$1"
+response_file=$(mktemp)
+curl -s "$url" > "$response_file"
+response_size=$(wc -c < "$response_file")
